6e0845ae8bcf89d3ba9e34a325c180e565d99285
914313670 <914313670@qq.com>
2019-11-21 00:07:46 +0800

    21

--- src/main/java/top/liujingyanghui/assignmentupload/controller/UserController.java
+++ src/main/java/top/liujingyanghui/assignmentupload/controller/UserController.java
@@ -5,10 +5,7 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.liujingyanghui.assignmentupload.entity.User;
 import top.liujingyanghui.assignmentupload.service.SchoolService;
 import top.liujingyanghui.assignmentupload.service.UserService;
@@ -50,5 +47,18 @@ public class UserController {
                     .eq(User::getRole, "ROLE_TEACHER").eq(User::getSchoolId, schoolId).like(User::getEmail, email).like(User::getNumber, number).like(User::getName, name));
         }
         return Result.success(page);
+    }
+
+    /**
+     * 老师信息修改
+     * @param user
+     * @return
+     */
+    @PutMapping("teacherUpdate")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Result update(@RequestBody User user) {
+        boolean update = userService.update(Wrappers.<User>lambdaUpdate().eq(User::getId, user.getId()).set(User::getName, user.getName()).set(User::getNumber, user.getNumber())
+                .set(User::getSchoolId, user.getSchoolId()));
+        return update?Result.success():Result.error("修改失败");
     }
 }
